2d236178917456a3e1e53b51c89be5d0ddebf212
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2022-01-13 10:36:59 +0530

    Adding React Dropdown list grouping video

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# grouping-items-in-react-dropdownlist
-A quick-start project that shows how to categorize the react dropdown list items using the grouping feature and customize it using templates. Also, learn how to add icons to the React Dropdown List items.  
+# Grouping Items in React Dropdown List
+
+A quick-start project that shows how to categorize the react dropdown list items using the grouping feature and customize it using templates. Also, learn how to add icons to the React Dropdown List items.
+
+Documentation: 
+
+https://ej2.syncfusion.com/react/documentation/drop-down-list/grouping/
+
+https://ej2.syncfusion.com/react/documentation/drop-down-list/how-to/icons-support/
+
+Online examples: https://ej2.syncfusion.com/react/demos/#/bootstrap5/drop-down-list/grouping-icon
+
+## Project prerequisites
+
+Make sure that you have the latest versions of NodeJS and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `grouping-items-in-react-dropdownlist` repository and then open it in Visual Studio Code. Now, simply install all the necessary react packages into your current project using the `npm install` command and run your project using the `npm start` command.
---
+++ src/data.js
@@ -0,0 +1,25 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.employees=[
+    { "Name": "Andrew Fuller", "EmployeeID": "7", "Designation": "Team Lead", "Country": "England" },
+    { "Name": "Anne Dodsworth", "EmployeeID": "1", "Designation": "Developer", "Country": "USA" },
+    { "Name": "Janet Leverling", "EmployeeID": "3", "Designation": "HR", "Country": "USA" },
+    { "Name": "Laura Callahan", "EmployeeID": "2", "Designation": "Product Manager", "Country": "USA" },
+    { "Name": "Margaret Peacock", "EmployeeID": "6", "Designation": "Developer", "Country": "USA" },
+    { "Name": "Michael Suyama", "EmployeeID": "9", "Designation": "Team Lead", "Country": "USA" },
+    { "Name": "Nancy Davolio", "EmployeeID": "4", "Designation": "Product Manager", "Country": "USA" },
+    { "Name": "Robert King", "EmployeeID": "8", "Designation": "Developer ", "Country": "England" },
+    { "Name": "Steven Buchanan", "EmployeeID": "10", "Designation": "CEO", "Country": "England" }
+]
+exports.socialMedia =[
+    { "Class": "facebook", "SocialMedia": "Facebook", "Id": "media1" },
+    { "Class": "google-plus", "SocialMedia": "Google Plus", "Id": "media2" },
+    { "Class": "instagram", "SocialMedia": "Instagram", "Id": "media3" },
+    { "Class": "linkedin", "SocialMedia": "LinkedIn", "Id": "media4" },
+    { "Class": "skype", "SocialMedia": "Skype", "Id": "media5" },
+    { "Class": "tumblr", "SocialMedia": "Tumblr", "Id": "media6" },
+    { "Class": "twitter", "SocialMedia": "Twitter", "Id": "media7" },
+    { "Class": "vimeo", "SocialMedia": "Vimeo", "Id": "media8" },
+    { "Class": "whatsapp", "SocialMedia": "WhatsApp", "Id": "media9" },
+    { "Class": "youtube", "SocialMedia": "YouTube", "Id": "media10" }
+];
